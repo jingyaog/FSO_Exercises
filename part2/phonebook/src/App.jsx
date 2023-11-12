9e0ecc4be6fc0de4e30ca33741a02363aaff9524
jingyaog <jingyaog@andrew.cmu.edu>
2023-11-12 14:24:41 -0800

    2.12 finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -45,6 +45,12 @@ const App = () => {
       number: newNumber,
       id: persons.length + 1
     }
+
+    axios
+      .post('http://localhost:3001/persons', person)
+      .then(response => {
+        console.log(response)
+      })
     setPersons(persons.concat(person))
     setNewName('')
     setNewNumber('')
