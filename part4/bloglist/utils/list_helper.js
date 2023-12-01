ec26617413521aa9d426721a7bade8359368f928
jingyaog <jingyaog@andrew.cmu.edu>
2023-12-01 10:59:47 -0800

    4.5 finished

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, cur) => acc + cur.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  const blog = blogs.reduce((acc, cur) => {
+    return cur.likes >= acc.likes ? cur : acc
+  }, blogs[0])
+
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
