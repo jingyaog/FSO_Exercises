c2052236ec3bf088576b3fec35a64c39590177d5
jingyaog <jingyaog@andrew.cmu.edu>
2023-12-03 18:59:41 -0800

    4.11 finished

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -52,6 +52,21 @@ describe('creating blogs', () => {
     const titles = blogsAtEnd.map(b => b.title)
     expect(titles).toContain('New Blog 0')
   })
+
+  test('if the likes property is missing it will default to 0', async () => {
+    const newBlog = {
+      title: "New Blog 1",
+      author: "Aaron 1",
+      url: "http://www.4301.com"
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+
+    expect(response.body.likes).toBe(0)
+  })
 })
 
 afterAll(async () => {
