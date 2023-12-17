6b17fca2e28065140247e103abd87d4f89dbef51
jingyaog <jingyaog@andrew.cmu.edu>
2023-12-17 15:35:20 -0800

    5.14 finished

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders the blog\'s title and author but not URL or number of likes', () => {
@@ -14,7 +15,7 @@ test('renders the blog\'s title and author but not URL or number of likes', () =
       name: 'Test'
     }
   }
-  const user = {
+  const blogUser = {
     username: 'Test user 1',
     name: 'Test'
   }
@@ -22,7 +23,7 @@ test('renders the blog\'s title and author but not URL or number of likes', () =
   const mockHandler = jest.fn()
 
   const { container } = render(
-    <Blog blog={blog} user={user} incrementLikes={mockHandler} deleteBlog={mockHandler} />
+    <Blog blog={blog} user={blogUser} incrementLikes={mockHandler} deleteBlog={mockHandler} />
   )
 
   const titleElement = screen.queryByText('Test blog 1', { exact: false })
@@ -34,5 +35,44 @@ test('renders the blog\'s title and author but not URL or number of likes', () =
   expect(urlDiv).not.toBeVisible()
 
   const likesDiv = container.querySelector('.likes')
+  expect(likesDiv).not.toBeVisible()
+})
+
+test('blog\'s URL and number of likes are shown when button is clicked', async () => {
+  const blog = {
+    title: 'Test blog 1',
+    author: 'Aaron',
+    url: 'http://www.test.com',
+    likes: 3,
+    user: {
+      username: 'Test user 1',
+      name: 'Test'
+    }
+  }
+  const blogUser = {
+    username: 'Test user 1',
+    name: 'Test'
+  }
+
+  const mockHandler = jest.fn()
+
+  const { container } = render(
+    <Blog blog={blog} user={blogUser} incrementLikes={mockHandler} deleteBlog={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const urlDiv = container.querySelector('.url')
+  const likesDiv = container.querySelector('.likes')
+
+  expect(urlDiv).toBeVisible()
+  expect(likesDiv).toBeVisible()
+
+  const hideButton = screen.getByText('hide')
+  await user.click(hideButton)
+
+  expect(urlDiv).not.toBeVisible()
   expect(likesDiv).not.toBeVisible()
 })
